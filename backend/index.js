7faadb723f8119ad07568a118b5fc7cc37b6b52a
sanjay <sanjaytomar717@gmail.com>
2024-01-15 06:55:01 +0530

    updated cors

--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,7 @@ import cors from "cors"
 const app = express()
 const PORT = process.env.PORT || 3000;
 app.use(express.json());
+app.options('*', cors());
 app.use(cors({
     origin: '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
